793077f8db1f9e4bdd79fe313ef98fc1de734336
SamiLumine <SamiGG15@gmail.com>
2023-11-15 13:50:25 +0100

    suppression contenu cpp et ajout contenu hpp

--- src/amogus.cpp
+++ src/amogus.cpp
@@ -1,25 +1,5 @@
 #include "../lib/include/amogus.hpp"
 
-class Amongus
-{
-    protected: 
-        double vision;
-        double interaction;
-        double vitesse;
-        bool weapon;
-    public:
-        /*getter*/
-        double get_vision() { return vision; }
-        double get_interaction() { return interaction; }
-        double get_vitesse() { return vitesse; }
-        bool get_weapon() { return weapon; }
-        /*setter*/
-        void set_vision(double v) { vision = v;}
-        void set_interaction(double interaction) { interaction = interaction;}
-        void set_vitesse(bool v) { vitesse = v;}
-        /*autre*/
-        void move()
-        {
-            /*code*/
-        }
-};
+
+
+
--- src/crewmate.cpp
+++ src/crewmate.cpp
@@ -1,15 +1,1 @@
 #include "../lib/include/crewmate.hpp"
-
-class Crewmate : public Amongus
-{
-    private:
-        int nb_task;
-        double proba;
-    public:
-        /*getter*/
-        int get_nb_task() { return nb_task; }
-        double get_proba() { return proba; }
-        /*setter*/
-        void set_nb_task(int nb_task) { nb_task = nb_task;}
-        void set_proba(double proba) { proba = proba;}
-};
