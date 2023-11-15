d4cfdd22f96c0be3381a1d7606bddd0c2b439c72
SamiLumine <SamiGG15@gmail.com>
2023-11-15 13:54:28 +0100

    ajout contenu hpp

---
+++ include/crewmate.hpp
@@ -0,0 +1,22 @@
+#ifndef DEF_CREWMATE
+#define DEF_CREWMATE
+
+#include "amogus.hpp"
+#include "task.hpp"
+
+class Crewmate : public Amogus
+{
+    private:
+        int nb_task;
+        double proba;
+    public:
+        /*getter*/
+        int get_nb_task();
+        double get_proba();
+        /*setter*/
+        void set_nb_task(int nb_task);
+        void set_proba(double proba);
+};
+
+
+#endif
