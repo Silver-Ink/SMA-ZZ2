97d2f764db971035e5f30670b3d4b1435f0099e6
SamiLumine <SamiGG15@gmail.com>
2023-11-22 15:10:54 +0100

    ajout d'une première fenêtre

--- main.cpp
+++ main.cpp
@@ -2,6 +2,15 @@
 
 int main()
 {
-    cout << "AMOGUS !" << endl;
-    return 0;
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+    InitWindow(screenWidth, screenHeight, "raylib [textures] example - image drawing");
+	SetTargetFPS(60);
+	while (!WindowShouldClose())
+	{
+		ClearBackground(BLACK);
+		EndDrawing();
+    }
+    CloseWindow();
+return 0;
 }
